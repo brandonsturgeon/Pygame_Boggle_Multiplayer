1f423e6363a8795541652c82dfcc3143faae42f0
BrandonSturgeonMarket <brandon@madebymarket.com>
2015-01-13 12:53:44 -0600

    Adds a button to rotate the board 90 degrees clockwise

--- Pygame_Boggle.py
+++ Pygame_Boggle.py
@@ -12,18 +12,23 @@ class Tile(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((100, 100))
         self.color = (182, 192, 210)
-        self.rect = pygame.Rect(pos, self.image.get_size())
+        self.pos = pos
+        self.rect = pygame.Rect(((pos[0]*120)+10, (pos[1]*120)+10), self.image.get_size())
         self.letter = letter
         self.letter_image = font.render(self.letter.upper(), 1, (255, 255, 255))
-        
+
         self.update()
-        
+
     def update(self):
         # Fill and then blit the letter
         self.image.fill(self.color)
         blit_x = (self.image.get_width() / 2) - (self.letter_image.get_width() / 2)
         blit_y = (self.image.get_height() / 2) - (self.letter_image.get_height() / 2)
         self.image.blit(self.letter_image, (blit_x, blit_y))
+
+    def rotate(self):
+        self.pos =  4-self.pos[1], self.pos[0]
+        self.rect = pygame.Rect(((self.pos[0]*120)+10, (self.pos[1]*120)+10), self.image.get_size())
 
 
 class Game():
@@ -56,6 +61,15 @@ class Game():
         self.new_game_button_rect = pygame.Rect((self.game_window.get_width() - self.new_game_button.get_width(),
                                                  self.game_window.get_height() - self.new_game_button.get_height()),
                                                 self.new_game_button.get_size())
+        self.rotate_button = pygame.Surface((100, 25))
+        self.rotate_button.fill((0, 0, 255))
+        self.rotate_text = self.word_font.render("Rotate", 1, (255, 0, 0))
+        self.rotate_button.blit(self.rotate_text, ((self.rotate_button.get_width()/2) -
+                                                   (self.rotate_text.get_width()/2),
+                                                   (self.rotate_button.get_height()/2) -
+                                                   (self.rotate_text.get_height()/2)))
+        self.rotate_button_rect = pygame.Rect((600, self.game_window.get_height() - self.new_game_button.get_height()),
+                                               self.rotate_button.get_size())
         self.words = []
 
         self.tiles = pygame.sprite.Group()
@@ -103,6 +117,11 @@ class Game():
                         self.gen_tiles()
                         self.game_loop()
 
+                    # Rotate board clockwise
+                    elif self.rotate_button_rect.collidepoint((self.mouse_x, self.mouse_y)):
+                        for t in self.tiles:
+                          t.rotate()
+
                 # Mouse is lifted up, check the word
                 if event.type == pygame.MOUSEBUTTONUP:
                     self.bound_box = pygame.Rect((0, 0), self.tile_container.get_size())
@@ -121,6 +140,7 @@ class Game():
             self.game_window.blit(self.tile_container, (0, 0))
             self.game_window.blit(self.word_container, (600, 0))
             self.game_window.blit(self.new_game_button, (self.new_game_button_rect.x, self.new_game_button_rect.y))
+            self.game_window.blit(self.rotate_button, (self.rotate_button_rect.x, self.rotate_button_rect.y))
             pygame.display.flip()
             self.clock.tick(60)
 
@@ -133,9 +153,9 @@ class Game():
         # Keeps track of all the letters we've used, puts a limit of 2 on each letter
         # Note that some consonants start at 1, so that we're even less likely to generate it
         letter_dict = {"q": 1, "x": 1, "a": -2, "e": -2, "i": -2, "o": -2, "u": -2}
-        y_val = 10
+        y_val = 0
         for y in range(5):
-            x_val = 10
+            x_val = 0
             for x in range(5):
 
                 # Limit 2 of each consonant, uses dictionaries.
@@ -155,8 +175,8 @@ class Game():
 
                 # Create the Tile
                 self.tiles.add(Tile((x_val, y_val), r_letter, font))
-                x_val += 120
-            y_val += 120
+                x_val += 1
+            y_val += 1
 
         print "Generation Complete!"
 
@@ -188,7 +208,7 @@ class Game():
             color = (255, 255, 255)
             if len(word) >= len(maxword):
                 color = (0, 255, 0)
-                
+
             render_font = self.word_font.render(word.capitalize()+": "+str(len(word)), 1, color)
             self.word_container.blit(render_font, (x_value, y_value))
 
